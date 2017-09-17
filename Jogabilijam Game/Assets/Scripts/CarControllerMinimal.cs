158e7aa2bef001853d81f97500b7cf21c923216a
GuiRokk <gui.rokk@gmail.com>
2017-09-17 12:53:49 -0300

    Sprites e Trigger Boost

--- Jogabilijam Game/Assets/Scripts/CarControllerMinimal.cs
+++ Jogabilijam Game/Assets/Scripts/CarControllerMinimal.cs
@@ -2,59 +2,79 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CarControllerMinimal : MonoBehaviour {
+public class CarControllerMinimal : MonoBehaviour
+{
 
-	private Rigidbody rb;
-	private Vector3 v3Front;
-	private Vector3 v3Back;
-	private Vector3 v3Left;
-	private Vector3 v3Right;
+    private Rigidbody rb;
+    private Vector3 v3Front;
+    private Vector3 v3Back;
+    private Vector3 v3Left;
+    private Vector3 v3Right;
 
-	public int accellScale;
-	public int reverseScale;
-	public int rotateScale;
-    public bool flagLEFT;
+    public int accellScale;
+    public int reverseScale;
+    public int rotateScale;
+    public int boost = 1;
 
-	// Use this for initialization
-	void Start () 
-	{
-		rb = GetComponent<Rigidbody>();
-		v3Front = new Vector3(0, 0, 1);
-		v3Back = new Vector3(0, 0, -1);
-		v3Left = new Vector3(0, -1, 0);
-		v3Right = new Vector3(0, 1, 0);
 
-	}
-	
-	// Update is called once per frame
-	void Update () 
-	{
-		MovementControl();
-	}
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        v3Front = new Vector3(0, 0, 1);
+        v3Back = new Vector3(0, 0, -1);
+        v3Left = new Vector3(0, -1, 0);
+        v3Right = new Vector3(0, 1, 0);
 
-	void MovementControl() 
-	{
-		if (Input.GetKey(KeyCode.W))
-		{
-			rb.AddRelativeForce(v3Front * accellScale);
-		}
-		if (Input.GetKey(KeyCode.S))
-		{
-			rb.AddRelativeForce(v3Back * reverseScale);
-		}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovementControl();
+    }
+
+    void MovementControl()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddRelativeForce(v3Front * accellScale * boost);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddRelativeForce(v3Back * reverseScale);
+        }
+
         if (Input.GetKey(KeyCode.A))
         {
             transform.Rotate(v3Left * Time.deltaTime * rotateScale);
-            flagLEFT = true;
         }
-        else {
-            flagLEFT = false;
-}
-		if (Input.GetKey(KeyCode.D))
-		{
-       	        transform.Rotate (v3Right * Time.deltaTime * rotateScale);
-               
-		}
 
-	}
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(v3Right * Time.deltaTime * rotateScale);
+        }
+    }
+
+    ///// trigger do Boost /////
+    void OnTriggerEnter(Collider other)
+    {
+
+        boost = 2;
+
+        StartCoroutine(StopBoost());
+
+    }
+
+    IEnumerator StopBoost() //tempo de boost
+    {
+        yield return new WaitForSeconds(1.3f);
+        boost = 1;
+
+    }
+
 }
+
+
+
---
+++ Jogabilijam Game/Assets/Scripts/Sprites.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sprites : MonoBehaviour
+{
+
+    public Sprite spUp, spDown, spLeft, spRight;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.GetComponent<SpriteRenderer>().sprite = spUp;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.W))
+            this.GetComponent<SpriteRenderer>().sprite = spUp;
+
+        if (Input.GetKey(KeyCode.S))
+            this.GetComponent<SpriteRenderer>().sprite = spDown;
+
+        if (Input.GetKey(KeyCode.D))
+            this.GetComponent<SpriteRenderer>().sprite = spRight;
+
+        if (Input.GetKey(KeyCode.A))
+            this.GetComponent<SpriteRenderer>().sprite = spLeft;
+
+
+
+
+
+    }
+}
