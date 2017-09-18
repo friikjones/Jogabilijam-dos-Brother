a4e50407b30d20f07754674d34fc51bf72bbf263
GuiRokk <gui.rokk@gmail.com>
2017-09-17 21:50:56 -0300

    Curvas melhores e "drift"

--- Jogabilijam Game/Assets/Scripts/CarControllerMinimal.cs
+++ Jogabilijam Game/Assets/Scripts/CarControllerMinimal.cs
@@ -5,37 +5,60 @@ using UnityEngine;
 public class CarControllerMinimal : MonoBehaviour
 {
 
+    private CharacterController controller;
+
     private Rigidbody rb;
     private Vector3 v3Front;
     private Vector3 v3Back;
     private Vector3 v3Left;
     private Vector3 v3Right;
+    private Vector3 v3Up;
 
     public int accellScale;
     public int reverseScale;
     public int rotateScale;
     public int boost = 1;
+    private int driftR, driftL;
+    private float gravidade=14f;
+
+    
 
 
     // Use this for initialization
     void Start()
     {
+        
+
+
         rb = GetComponent<Rigidbody>();
         v3Front = new Vector3(0, 0, 1);
         v3Back = new Vector3(0, 0, -1);
         v3Left = new Vector3(0, -1, 0);
         v3Right = new Vector3(0, 1, 0);
-
+        v3Up = new Vector3(0, 1, 0);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         MovementControl();
     }
 
     void MovementControl()
     {
+        //PULO///////
+       /* if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddRelativeForce(v3Up * 600);
+
+        }
+        else {
+            rb.AddRelativeForce(v3Up * -30);
+        }
+        */
+
         if (Input.GetKey(KeyCode.W))
         {
             rb.AddRelativeForce(v3Front * accellScale * boost);
@@ -46,24 +69,95 @@ public class CarControllerMinimal : MonoBehaviour
             rb.AddRelativeForce(v3Back * reverseScale);
         }
 
+
+
         if (Input.GetKey(KeyCode.A))
         {
+
             transform.Rotate(v3Left * Time.deltaTime * rotateScale);
+            driftL = driftL + 1;
+
+            if (driftL > 22)
+            {
+                transform.Rotate(v3Left * Time.deltaTime * (rotateScale - 110));
+
+
+            }
+            if (driftL > 35)
+            {
+                transform.Rotate(v3Left * Time.deltaTime * (rotateScale - 120));
+            }
+
         }
+        else
+        {
+            driftL = 0;
+        }
+
+
+    
+
 
         if (Input.GetKey(KeyCode.D))
         {
             transform.Rotate(v3Right * Time.deltaTime * rotateScale);
+
+            driftR = driftR + 1;
+
+            if (driftR > 22)
+            {
+                transform.Rotate(v3Right * Time.deltaTime * (rotateScale - 110));
+
+
+            }
+            if (driftR > 35)
+            {
+                transform.Rotate(v3Right * Time.deltaTime * (rotateScale - 120));
+            }
+
         }
+        else
+        {
+            driftR = 0;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
     }
 
+
+
     ///// trigger do Boost /////
-    void OnTriggerEnter(Collider other)
+    void OnTriggerEnter(Collider collider)
     {
 
-        boost = 2;
+        if (collider.gameObject.name == "Boost Trigger")
+        {
 
-        StartCoroutine(StopBoost());
+            boost = 2;
+
+            StartCoroutine(StopBoost());
+
+        }
+
+       
+
 
     }
 
@@ -73,6 +167,16 @@ public class CarControllerMinimal : MonoBehaviour
         boost = 1;
 
     }
+
+
+
+  
+
+
+
+
+
+
 
 }
 
--- Jogabilijam Game/Assets/Scripts/Sprites.cs
+++ Jogabilijam Game/Assets/Scripts/Sprites.cs
@@ -5,13 +5,22 @@ using UnityEngine;
 public class Sprites : MonoBehaviour
 {
 
-    public Sprite spUp, spDown, spLeft, spRight;
+    public Sprite spUp, spDown, spLeft, spRight, spHoldRight, spSuperHoldRight, spHoldLeft, spSuperHoldLeft;
+    private int driftL, driftR;
+
+
 
     // Use this for initialization
     void Start()
     {
+
+
         this.GetComponent<SpriteRenderer>().sprite = spUp;
+
     }
+
+
+
 
     // Update is called once per frame
     void Update()
@@ -23,14 +32,57 @@ public class Sprites : MonoBehaviour
         if (Input.GetKey(KeyCode.S))
             this.GetComponent<SpriteRenderer>().sprite = spDown;
 
-        if (Input.GetKey(KeyCode.D))
-            this.GetComponent<SpriteRenderer>().sprite = spRight;
+
 
         if (Input.GetKey(KeyCode.A))
+        {
+
             this.GetComponent<SpriteRenderer>().sprite = spLeft;
 
+            driftR = driftR + 1;
+
+            if (driftR > 25)
+            {
+                this.GetComponent<SpriteRenderer>().sprite = spHoldLeft;
 
 
+            }
+            if (driftR > 35)
+            {
+                this.GetComponent<SpriteRenderer>().sprite = spSuperHoldLeft;
+            }
+
+        }
+        else
+        {
+            driftR = 0;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.GetComponent<SpriteRenderer>().sprite = spRight;
+
+            driftL = driftL + 1;
+
+            if (driftL > 25)
+            {
+                this.GetComponent<SpriteRenderer>().sprite = spHoldRight;
+
+
+            }
+            if (driftL > 35)
+            {
+                this.GetComponent<SpriteRenderer>().sprite = spSuperHoldRight;
+
+
+            }
+
+        }
+        else
+        {
+
+            driftL = 0;
+        }
 
 
     }
