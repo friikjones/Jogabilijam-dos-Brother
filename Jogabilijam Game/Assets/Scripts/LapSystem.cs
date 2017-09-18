a1af02ee1dff09250dacc16e1129acbbe66d0dc5
GuiRokk <gui.rokk@gmail.com>
2017-09-18 20:40:13 -0300

    AGORA VAI NESSA MERDA

---
+++ Jogabilijam Game/Assets/Scripts/LapSystem.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LapSystem : MonoBehaviour
+{
+
+    public int n_checks;
+    public int prox_check;
+    public int n_volta;
+    public int volta_atual;
+
+    void Start()
+    {
+
+
+        n_checks = GameObject.Find("Checkpoints").transform.childCount; //contagem do numero de check points
+        prox_check = 1;
+        n_volta = 3;
+        volta_atual = 1;
+    }
+
+    void Update()
+    {
+        if (prox_check > n_checks)   //verifica se passou pelos checks em ordem
+        {
+            volta_atual++;
+            prox_check = 1;
+
+        }
+
+        if (volta_atual == n_volta)
+        {
+
+            Debug.Log("Finish");
+
+        }
+
+
+    }
+
+    private void OnTriggerEnter(Collider check_col)
+    {
+        if (check_col.name == prox_check.ToString())   //se passar pelo check, incrementa
+        {
+
+            prox_check++;
+        }
+    }
+}
---
+++ Jogabilijam Game/Assets/Scripts/Ranking.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ranking : MonoBehaviour
+{
+
+    public GameObject car;
+    public GameObject check1, check2, check3;
+    public float dist1, dist2, dist3;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+        dist1 = Vector3.Distance(car.transform.position, check1.transform.position);
+        dist2 = Vector3.Distance(car.transform.position, check2.transform.position);
+        dist3 = Vector3.Distance(car.transform.position, check3.transform.position);
+    }
+}
