f145dd2faba86ed3a48731db61d371411f5e2919
Alexandre Jones <ale.mullerjones@gmail.com>
2017-09-24 22:05:17 -0300

    Char Select 0.2

---
+++ Jogabilijam Game/Assets/Scripts/CharSelectInfo.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CharSelectInfo : MonoBehaviour {
+
+	//private static int index;
+	private Canvas canvas;
+	private CharSelectDTOScript DTO;
+
+	// Use this for initialization
+	void Start () {
+
+		canvas = GameObject.Find("EmptyThingy").GetComponentInChildren<Canvas>();
+		DTO = GameObject.Find("CharSelectDTO").GetComponent<CharSelectDTOScript>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ButtonPressed(int butIndex){
+		//index = butIndex;
+		DTO.setIndex(butIndex);
+		SceneManager.LoadScene ("Mockup");
+		canvas.enabled = false;
+	}
+}
